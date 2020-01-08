a51f186098f01dcfbab08b0dc8e2f5d394339400
Adam Shortsleeves <ashortsleeves@gmail.com>
2020-01-08 10:09:37 -0500

    importing sage9 scss styling

--- assets/application.js
+++
@@ -1,9 +0,0 @@
-// Put your applicaiton javascript here
-snippetOptions: {
-    rule: {
-        match: /<\/body>/i,
-        fn: function (snippet, match) {
-            return snippet + match;
-        }
-    }
-}
